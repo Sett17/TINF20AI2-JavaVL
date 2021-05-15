45e75f784ca09642b8db9e6b80c19ae7f69df502
Sett17 <raik.rohde@gmail.com>
2021-05-15 22:12:34 +0200

    deleted carKt; refactored carJ -> car for clarity

--- carKt/src/Cabrio.kt
+++
@@ -1,7 +0,0 @@
-class Cabrio(hersteller: String, typ: String, breite: Double, color: CarColor) : Car(hersteller, typ, breite, color) {
-
-    override fun drive() {
-        super.drive()
-        println("\tohne dach!")
-    }
-}
